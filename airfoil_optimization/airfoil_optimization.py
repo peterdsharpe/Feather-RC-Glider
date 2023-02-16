152bd536669f9289b5d2c1778bd81a8ed75592ff
Peter Sharpe <pds@mit.edu>
2023-02-16 00:50:11 -0500

    rework design optimization

--- airfoil_optimization/airfoil_optimization.py
+++ airfoil_optimization/airfoil_optimization.py
@@ -1,22 +1,43 @@
 from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.algorithms.moo.unsga3 import UNSGA3
+from pymoo.util.ref_dirs import get_reference_directions
 # from pymoo.problems import get_problem
 from pymoo.core.problem import Problem, ElementwiseProblem, ElementwiseEvaluationFunction
 from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 from pymoo.termination import get_termination
+# from dask.distributed import Client
+import multiprocessing
+from multiprocessing.pool import ThreadPool
+from pymoo.core.problem import StarmapParallelization
+from pymoo.core.callback import Callback
 
 import aerosandbox as asb
 import aerosandbox.numpy as np
 from aerosandbox.geometry.airfoil import get_kulfan_coordinates
 
+import matplotlib
 
+matplotlib.rc('figure', max_open_warning=0)
 import matplotlib.pyplot as plt
 import aerosandbox.tools.pretty_plots as p
 
-# fig, ax = plt.subplots()
+op_point = asb.OperatingPoint(
+    velocity=4.975614658280702
+)
+wing_mean_aerodynamic_chord = 0.11079155799884116
+mass = 0.08152734674206971
 
-resolution = 20
-Re = 35e3
+resolution = 16
+Re = op_point.reynolds(wing_mean_aerodynamic_chord)
+n_procs = 16
+
+CL_trim = (9.81 * mass) / (1 * wing_mean_aerodynamic_chord) / op_point.dynamic_pressure()
+
+alpha_range = 2.5  # +- 2 deg
+n_alphas = resolution
+
+population_size = 32
 
 def unpack_x(x):
     alpha = x[0]
@@ -42,82 +63,175 @@ def make_airfoil(x):
             lower_weights=x_camber - x_thickness,
             enforce_continuous_LE_radius=False,
             n_points_per_side=80,
+            TE_thickness=400e-6 / 150e-3
         )
     )
 
 
 class MyProblem(ElementwiseProblem):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(
             n_var=len(pack_x(0, 0, 0)),  # geometry + alpha
             n_obj=2,  # L/D, Cm,
             xl=pack_x(
-                x_camber=-0.1,
-                x_thickness=0,
-                alpha=-5
+                alpha=0,
+                x_camber=-0.4,
+                x_thickness=np.concatenate((
+                    [0.04],
+                    -0.1 * np.ones(resolution - 1)
+                )),
             ),
             xu=pack_x(
-                x_camber=0.40,
+                alpha=12,
+                x_camber=0.60,
                 x_thickness=0.40,
-                alpha=15
-            )
+            ),
+            **kwargs
         )
 
-    def _evaluate(self, x, out, *args, **kwargs):
-        # print(repr(x))
+    def _evaluate(self, x, out, debug=False, *args, **kwargs):
+
+        with open("x.log", "a") as f:
+            f.write("\nnp." + repr(x) + ',')
 
         alpha, x_camber, x_thickness = unpack_x(x)
 
         af = make_airfoil(x)
 
-        # fig.clear()
-        # af.draw()
-        # plt.show()
-        # plt.pause(1e-3)
-
         try:
+            alphas = np.linspace(alpha - alpha_range, alpha + alpha_range, n_alphas)
+
             xf = asb.XFoil(
                 airfoil=af,
                 Re=Re,
                 mach=0,
                 xfoil_repanel=False,
                 # verbose=True,
-                max_iter=20
+                max_iter=100,
+                timeout=10,
             )
-            aero = xf.alpha(alpha=alpha)
 
-            LD = aero["CL"] / aero["CD"]
-            Cm = aero["CM"]
-            if len(LD) == 0:
-                raise FileNotFoundError
-            try:
-                LD = float(LD)
-                Cm = float(Cm)
-            except TypeError:
-                print(repr(aero))
-                raise TypeError
+            aero = xf.alpha(
+                alpha=alphas,
+                start_at=alpha
+            )
 
-            print("\t", LD, Cm)
-        except FileNotFoundError:
+            if debug:
+                print(aero)
+
+            aero['CD'] = np.where(
+                aero["CDp"] <= 0,
+                1,
+                aero["CD"]
+            )
+
+            LDs = aero["CL"] / aero["CD"]
+            Cms = aero["CM"]
+
+            if len(LDs) == 0:
+                raise Exception()
+
+            n_nans = len(alphas) - len(LDs)
+
+            LD = np.mean(np.concatenate((
+                LDs,
+                np.zeros(n_nans)
+            )))
+            Cm = np.mean(np.concatenate((
+                Cms,
+                -1 * np.ones(n_nans)
+            )))
+
+            LD = float(LD)
+            Cm = float(Cm)
+
+        except BaseException as e:
+            print(e)
             LD = 0
-            Cm = -1
+            Cm = -10
+            n_nans = len(alphas)
+
+        print(f"\t\tLD = {LD:8.2f} | Cm = {Cm:8.3f} | n_nans = {n_nans}")
 
         out["F"] = [-LD, -Cm]
 
-res = minimize(
-    problem=MyProblem(),
-    algorithm=NSGA2(pop_size=30),
-    termination=get_termination('time', "06:00:00"),
-    seed=1,
-    verbose=True,
+
+class MyCallback(Callback):
+    def __init__(self):
+        super().__init__()
+
+    def notify(self, algorithm):
+        if algorithm.n_iter % 10 == 1:
+            fig, ax = plt.subplots()
+            best_LD_x = algorithm.pop.get("X")[np.argmin(algorithm.pop.get("F")[:, 0]), :]
+            af = make_airfoil(best_LD_x)
+            af.draw()
+            plt.show()
+
+
+pool = ThreadPool(n_procs)
+runner = StarmapParallelization(pool.starmap)
+
+problem = MyProblem(
+    elementwise_runner=runner
 )
 
-fig, ax = plt.subplots()
-LDs = -res.F[:, 0]
-Cms = -res.F[:, 1]
-plt.plot(Cms, LDs, ".")
-p.show_plot(
-    f"Airfoil Pareto @ $Re = {Re}",
-    "$L/D$",
-    "$C_m$"
-)
+# algorithm = UNSGA3(
+#     ref_dirs=get_reference_directions('das-dennis', 2, n_partitions=population_size),
+# )
+
+algorithm = NSGA2(pop_size=population_size)
+
+termination = get_termination('time', "00:40:00")
+
+callback = MyCallback()
+
+if __name__ == '__main__':
+    res = minimize(
+        problem=problem,
+        algorithm=algorithm,
+        termination=termination,  # 7:29
+        callback=callback,
+        seed=0,
+        verbose=True,
+        save_history=True,
+    )
+
+    order = np.argsort(-res.F[:, 0])
+    afs = [
+        make_airfoil(res.X[i, :])
+        for i in order
+    ]
+    alphas = res.X[order, 0]
+    LDs = -res.F[order, 0]
+    Cms = -res.F[order, 1]
+
+    with open("res.txt", "w+") as f:
+        f.write("X = np." + repr(
+            res.X[order, :]
+        ))
+
+    fig, ax = plt.subplots()
+    plt.plot(Cms, LDs, ".")
+    p.show_plot(
+        f"Airfoil Pareto @ $Re = {Re:.0f}$",
+        "$C_m$",
+        "$L/D$",
+    )
+
+    fig, ax = plt.subplots(figsize=(9, 3))
+    colors = plt.cm.get_cmap('rainbow')(np.linspace(0, 1, len(afs)))
+    for af, color in zip(afs, colors):
+        plt.plot(
+            af.x(), af.y(), "-",
+            color=color,
+            alpha=0.3
+        )
+    p.equal()
+    p.show_plot()
+
+    fig, ax = plt.subplots()
+    xf = asb.XFoil(
+        airfoil=afs[-1],
+
+    )
--- feather.py
+++ feather.py
@@ -1,124 +1,210 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
 import aerosandbox.tools.units as u
+import pandas as pd
+import copy
 
-opti = asb.Opti()
+opti = asb.Opti(
+    # variable_categories_to_freeze="all",
+    # freeze_style="float"
+)
 
-wing_method = '3d printing'
+make_plots = True
 
-# span = 999e-3
-# span = opti.variable(init_guess=1, lower_bound=0.1)
-span = opti.parameter(1.000)
-# root_chord = opti.parameter(156.5e-3) #156.5e-3
-root_chord = opti.variable(156.5e-3, lower_bound=0) #156.5e-3
-# wing_airfoil = asb.Airfoil("s1091")
-wing_airfoil = asb.Airfoil("ag13")
-wing_airfoil.generate_polars(
-    cache_filename=f"cache/{wing_airfoil.name}.json",
-    xfoil_kwargs=dict(
-        xfoil_repanel=False
+##### Section: Parameters
+
+# wing_method = '3d printing'
+wing_method = 'foam'
+
+wing_span = 1
+wing_dihedral_angle_deg = 11
+vtail_dihedral_angle_deg= 37
+
+airfoils = {
+    name: asb.Airfoil(
+        name=name,
+    ) for name in [
+        "ag04",
+        # "ag09",
+        "ag13",
+        "naca0008"
+    ]
+}
+
+for v in airfoils.values():
+    v.generate_polars(
+        cache_filename=f"cache/{v.name}.json",
+        alphas=np.linspace(-10, 10, 21)
+    )
+
+##### Section: Vehicle Overall Specs
+
+op_point = asb.OperatingPoint(
+    velocity=opti.variable(
+        init_guess=14,
+        lower_bound=1,
+        log_transform=True
+    ),
+    alpha=opti.variable(
+        init_guess=0,
+        lower_bound=-10,
+        upper_bound=10
     )
 )
 
-tail_airfoil = asb.Airfoil("naca0008")
-tail_airfoil.generate_polars(
-    cache_filename=f"cache/{tail_airfoil.name}.json",
-    xfoil_kwargs=dict(
-        xfoil_repanel=True
-    )
+design_mass_TOGW = opti.variable(
+    init_guess=0.1,
+    lower_bound=1e-3
+)
+design_mass_TOGW = np.maximum(design_mass_TOGW, 1e-3)
+
+LD_cruise = opti.variable(
+    init_guess=15,
+    lower_bound=0.1,
+    log_transform=True
+)
+
+g = 9.81
+
+target_climb_angle = 45  # degrees
+
+thrust_cruise = (
+        design_mass_TOGW * g / LD_cruise
+)
+
+thrust_climb = (
+        design_mass_TOGW * g / LD_cruise +
+        design_mass_TOGW * g * np.sind(target_climb_angle)
+)
+
+##### Section: Vehicle Definition
+
+"""
+Coordinate system:
+
+Geometry axes. Datum (0, 0, 0) is coincident with the quarter-chord-point of the centerline cross section of the main 
+wing.
+
+"""
+
+### Define x-stations
+x_nose = opti.variable(
+    init_guess=-0.1,
+    upper_bound=1e-3,
+)
+x_tail = opti.variable(
+    init_guess=0.7,
+    lower_bound=1e-3
+)
+
+### Wing
+wing_root_chord = opti.variable(
+    init_guess=0.15,
+    lower_bound=1e-3
 )
 
 
-def chord(y):
-    spanfrac = y / (span / 2)
+def wing_rot(xyz):
+    dihedral_rot = np.rotation_matrix_3D(
+        angle=np.radians(wing_dihedral_angle_deg),
+        axis="X"
+    )
+
+    return dihedral_rot @ np.array(xyz)
+
+
+def wing_chord(y):
+    spanfrac = y / (wing_span / 2)
     chordfrac = 1 - 0.4 * spanfrac - 0.47 * (1 - (1 - spanfrac ** 2 + 1e-16) ** 0.5)
 
     # c_over_c_root = 0.1 + 0.9 * (1 - (y / half_span) ** 2) ** 0.5
-    return chordfrac * root_chord
+    return chordfrac * wing_root_chord
 
 
-def z(y):
-    spanfrac = y / (span / 2)
-    return y * np.tand(11)
-    # return spanfrac ** 2 * np.tand(12)
-    # return np.softmax(0, 2 * spanfrac - 1, hardness=7) * np.tand(6)
+def wing_twist(y):
+    return np.zeros_like(y)
 
 
-ys = np.sinspace(0, span / 2, 10, reverse_spacing=True)
-cs = chord(ys)
-zs = z(ys)
+wing_ys = np.sinspace(
+    0,
+    wing_span / 2,
+    11,
+    reverse_spacing=True
+)
 
 wing = asb.Wing(
     name="Main Wing",
     symmetric=True,
     xsecs=[
         asb.WingXSec(
-            xyz_le=[cs[0] - cs[i], ys[i], zs[i]],
-            chord=cs[i],
-            airfoil=wing_airfoil,
+            xyz_le=wing_rot([
+                -wing_chord(wing_ys[i]),
+                wing_ys[i],
+                0
+            ]),
+            chord=wing_chord(wing_ys[i]),
+            airfoil=airfoils["ag13"],
+            twist=wing_twist(wing_ys[i]),
         )
-        for i in range(np.length(ys))
+        for i in range(np.length(wing_ys))
     ]
+).translate([
+    0.75 * wing_root_chord,
+    0,
+    0
+])
+
+vtail_twist = opti.variable(init_guess=0, lower_bound=-15, upper_bound=15)
+
+vtail_shape_data = pd.DataFrame(
+    {
+        "x_le_inches" : [0, 0.28, 0.63, 0.93, 1.25, 1.75],
+        "y_le_inches" : [0, 2, 4, 5, 5.58, 6],
+        "chord_inches": [2.75, 2.50, 2.06, 1.72, 1.36, 0.75]
+    }
 )
 
-nose_length = 4 * u.inch  # Wing LE to Nose
-tail_length = 19 * u.inch  # Wing LE to Tail LE
+
+def vtail_rot(xyz):
+    dihedral_rot = np.rotation_matrix_3D(
+        angle=np.radians(vtail_dihedral_angle_deg),
+        axis="X"
+    )
+
+    return dihedral_rot @ np.array(xyz)
+
 
 vtail = asb.Wing(
     name="VTail",
     symmetric=True,
     xsecs=[
         asb.WingXSec(
-            chord=2.75 * u.inch,
-        ),
-        asb.WingXSec(
-            xyz_le=[0.28 * u.inch, 2 * u.inch, 0],
-            chord=2.50 * u.inch,
-        ),
-        asb.WingXSec(
-            xyz_le=[0.63 * u.inch, 4 * u.inch, 0],
-            chord=2.06 * u.inch,
-        ),
-        asb.WingXSec(
-            xyz_le=[0.93 * u.inch, 5 * u.inch, 0],
-            chord=1.72 * u.inch,
-        ),
-        asb.WingXSec(
-            xyz_le=[1.23 * u.inch, 5.58 * u.inch, 0],
-            chord=1.36 * u.inch,
-        ),
-        asb.WingXSec(
-            xyz_le=[1.75 * u.inch, 6 * u.inch, 0],
-            chord=0.75 * u.inch,
+            xyz_le=vtail_rot([
+                row["x_le_inches"] * u.inch,
+                row["y_le_inches"] * u.inch,
+                0
+            ]),
+            chord=row["chord_inches"] * u.inch,
+            twist=vtail_twist,
+            airfoil=airfoils["naca0008"]
         )
+        for i, row in vtail_shape_data.iterrows()
     ]
 ).translate([
-    tail_length,
+    x_tail - 0.7 * vtail_shape_data.iloc[0, :]["chord_inches"] * u.inch,
     0,
     0
 ])
-# vtail_twist=-7
-vtail_twist = opti.variable(init_guess=0, lower_bound=-30, upper_bound=30)
-
-for i in range(len(vtail.xsecs)):
-    xyz_le_flat = vtail.xsecs[i].xyz_le
-    vtail.xsecs[i].xyz_le = np.array([
-        xyz_le_flat[0],
-        np.cosd(37) * xyz_le_flat[1],
-        np.sind(37) * xyz_le_flat[1]
-    ])
-    vtail.xsecs[i].airfoil = tail_airfoil
-    vtail.xsecs[i].twist=vtail_twist
 
 fuselage = asb.Fuselage(
     name="Fuse",
     xsecs=[
         asb.FuselageXSec(
-            xyz_c=[-nose_length, 0, 0],
+            xyz_c=[x_nose, 0, 0],
             radius=7e-3 / 2
         ),
         asb.FuselageXSec(
-            xyz_c=[tail_length, 0, 0],
+            xyz_c=[x_tail, 0, 0],
             radius=7e-3 / 2
         )
     ]
@@ -133,122 +219,266 @@ airplane = asb.Airplane(
     fuselages=[fuselage]
 )
 
+##### Section: Internal Geometry and Weights
+
+mass_props = {}
+
+### Lifting bodies
 if wing_method == '3d printing':
-    wing_mass = asb.MassProperties(
-        mass=(14e-3 / (0.200 * 0.150)) * wing.area()
+    # mass_props['wing'] = asb.mass_properties_from_radius_of_gyration(
+    #     mass=(12e-3 / (0.200 * 0.150)) * wing.area(),
+    #     x_cg=
+    # )
+    raise ValueError
+
+elif wing_method == 'foam':
+    # density = 20.8 # kg/m^3, for Foamular 150
+    # density = 38.06 # kg/m^3, for hard blue foam
+    # density = 4 * u.lbm / u.foot ** 3
+    density = 2 * u.lbm / u.foot ** 3
+    mass_props['wing'] = asb.mass_properties_from_radius_of_gyration(
+        mass=wing.volume() * density,
+        x_cg=(0.500 - 0.25) * wing_root_chord,
+        z_cg=(0.03591) * (
+                np.sind(wing_dihedral_angle_deg) / np.sind(11)
+        ) * (
+                     wing_span / 1
+             ),
     )
 elif wing_method == 'elf':
-    wing_mass = asb.MassProperties(
-        mass=0.0506 * span ** 2.09,
-        x_cg=0.5 * root_chord
-    )
+    # wing_mass = asb.MassProperties(
+    #     mass=0.0506 * wing_span ** 2.09,
+    #     x_cg=0.5 * root_chord
+    # )
+    raise ValueError
 
-tail_mass = asb.MassProperties(
-    mass=5e-3 * span ** 2.09,
-    x_cg=vtail.xsecs[0].xyz_le[0] + vtail.xsecs[0].chord / 2
+mass_props["tail_surfaces"] = asb.mass_properties_from_radius_of_gyration(
+    mass=vtail.volume() * 80,
+    x_cg=vtail.xsecs[0].xyz_le[0] + 0.50 * vtail.xsecs[0].chord,
 )
 
-pod_mass = asb.MassProperties(
-    mass=7e-3,
-    x_cg=(-nose_length + wing.xsecs[0].chord) / 2
-)
-
-control_rods_mass = asb.MassProperties(
+mass_props["linkages"] = asb.MassProperties(
     mass=1e-3,
-    x_cg=tail_length / 2
+    x_cg=x_tail / 2
 )
 
-avionics_mass = asb.MassProperties(
-    mass=11.00e-3,
-    x_cg=-nose_length + 1.5 * u.inch
+### Pod and avionics
+
+mass_props["motor"] = asb.mass_properties_from_radius_of_gyration(
+    mass=4.49e-3,  # EX1103 - kv 6000, weight includes 3-phase wiring
+    x_cg=x_nose - 0.3 * u.inch
+)
+mass_props["motor_bolts"] = asb.mass_properties_from_radius_of_gyration(
+    mass=4 * 0.075e-3,
+    x_cg=x_nose
+)
+
+mass_props["propeller"] = asb.mass_properties_from_radius_of_gyration(
+    mass=1.54e-3,
+    x_cg=x_nose - 0.7 * u.inch
+)
+
+mass_props["propeller_band"] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.06e-3,
+    x_cg=mass_props["propeller"].x_cg
+)
+
+mass_props["flight_computer"] = asb.mass_properties_from_radius_of_gyration(
+    mass=4.30e-3,
+    x_cg=x_nose + 2 * u.inch + (1.3 * u.inch) / 2
 )  # Includes RX, 2 linear servos, ESC, BLDC motor, prop
 # https://www.buzzardmodels.com/4gram1spro-brick
 
-battery_mass = asb.MassProperties(
-    mass=4.56e-3,
-    x_cg=-20e-3
-)  # 1S 150 mAh
+mass_props["battery"] = asb.mass_properties_from_radius_of_gyration(
+    mass=4.61e-3,
+    x_cg=x_nose + 2 * u.inch
+)
 
-boom_mass = asb.MassProperties(
-    mass=7.0e-3 * ((nose_length + tail_length) / 826e-3) * span,
-    x_cg=826e-3 / 2
+mass_props["pod"] = asb.MassProperties(
+    mass=7e-3,
+    x_cg=(x_nose + 0.75 * wing_root_chord) / 2
+)
+
+mass_props["ballast"] = asb.mass_properties_from_radius_of_gyration(
+    mass=opti.variable(init_guess=0, lower_bound=0),
+    x_cg=opti.variable(init_guess=0, lower_bound=x_nose, upper_bound=x_tail),
+)
+
+### Boom
+
+mass_props["boom"] = asb.mass_properties_from_radius_of_gyration(
+    mass=7.0e-3 * ((x_tail - x_nose) / 826e-3),
+    x_cg=(x_nose + x_tail) / 2
 )  # https://www.buzzardmodels.com/p000
 
-ballast_mass = asb.MassProperties(
-    mass=opti.variable(init_guess=0., lower_bound=0.,)
-)
+### Summation
+mass_props_TOGW = asb.MassProperties(mass=0)
+for k, v in mass_props.items():
+    mass_props_TOGW = mass_props_TOGW + v
 
-mass_props = (
-        wing_mass +
-        tail_mass +
-        pod_mass +
-        control_rods_mass +
-        avionics_mass +
-        battery_mass +
-        boom_mass +
-        ballast_mass
-)
+### Add glue weight
+mass_props['glue_weight'] = mass_props_TOGW * 0.08
+mass_props_TOGW += mass_props['glue_weight']
 
-airspeed = opti.variable(init_guess=5, lower_bound=0)
-alpha = opti.variable(init_guess=4.5, lower_bound=-15, upper_bound=15)
-op_point = asb.OperatingPoint(
-    velocity=airspeed,
-    alpha=alpha
-)
+##### Section: Aerodynamics
 
-analysis = asb.AeroBuildup(
-    airplane,
-    op_point,
-    xyz_ref=mass_props.xyz_cg
+ab = asb.AeroBuildup(
+    airplane=airplane,
+    op_point=op_point,
+    xyz_ref=mass_props_TOGW.xyz_cg
 )
-aero = analysis.run()
+aero = ab.run_with_stability_derivatives(
+    alpha=True,
+    beta=True,
+    p=False,
+    q=False,
+    r=False,
+)
 
 opti.subject_to([
-    aero["L"] == 9.81 * mass_props.mass,
-    aero["Cm"] == 0
+    aero["L"] >= 9.81 * mass_props_TOGW.mass,
+    aero["Cm"] == 0,
 ])
 
 LD = aero["L"] / aero["D"]
 power_loss = aero["D"] * op_point.velocity
-sink_rate = power_loss / 9.81 / mass_props.mass
+sink_rate = power_loss / 9.81 / mass_props_TOGW.mass
 
-opti.minimize(sink_rate)
+##### Section: Stability
+static_margin = (aero["x_np"] - mass_props_TOGW.x_cg) / wing.mean_aerodynamic_chord()
 
+opti.subject_to(
+    static_margin == 0.08
+)
 
-# try:
-#     sol = opti.solve()
-# except RuntimeError as e:
-#     print(e)
-#     sol = opti.debug
-# s = lambda x: sol.value(x)
+##### Section: Finalize Optimization Problem
+objective = sink_rate / 0.3 + mass_props_TOGW.mass / 0.100 * 0.1
+penalty = (mass_props["ballast"].x_cg / 1e3) ** 2
 
-@np.vectorize
-def get_sols(input_value=None):
-    if input_value is not None:
-        opti.set_value(root_chord, input_value)
+opti.minimize(objective + penalty)
+
+### Additional constraint
+opti.subject_to([
+    LD_cruise == LD,
+    design_mass_TOGW == mass_props_TOGW.mass
+])
+
+opti.subject_to([
+    x_nose < -0.25 * wing_root_chord - 0.5 * u.inch,  # propeller must extend in front of wing
+    x_tail - x_nose < 0.826,  # due to the length of carbon tube I have
+    vtail.area() * np.cosd(vtail_dihedral_angle_deg) ** 2 * x_tail / (wing.area() * wing.mean_aerodynamic_chord()) > 0.25
+])
+
+if __name__ == '__main__':
     try:
-        sol = opti.solve(verbose=False, max_iter=50)
-        opti.set_initial_from_sol(sol)
+        sol = opti.solve()
     except RuntimeError:
         sol = opti.debug
-    return sol
+    s = lambda x: sol.value(x)
 
-sol = get_sols()
+    airplane.substitute_solution(sol)
+    op_point.substitute_solution(sol)
+    # dyn.substitute_solution(sol)
+    mass_props_TOGW.substitute_solution(sol)
 
-# inputs = np.linspace(0.05, 0.4, 10)
-# sols = get_sols(inputs)
+    for v in mass_props.values():
+        v.substitute_solution(sol)
 
-import matplotlib.pyplot as plt
-import aerosandbox.tools.pretty_plots as p
+    aero = {
+        k: s(v)
+        for k, v in aero.items() if not isinstance(v, list)
+    }
 
-def s(x):
-    return sol.value(x)
+    wing_lowres = copy.deepcopy(wing)
+    xsecs_to_keep = np.arange(len(wing.xsecs)) % 2 == 0
+    xsecs_to_keep[0] = True
+    xsecs_to_keep[-1] = True
+    wing_lowres.xsecs = np.array(wing_lowres.xsecs)[xsecs_to_keep]
 
-def ss(x):
-    return np.array([sol.value(x) if sol.stats()['success'] else np.nan for sol in sols])
+    avl_aero = asb.AVL(
+        airplane=asb.Airplane(
+            wings=[
+                wing_lowres,
+                vtail,
+            ],
+            fuselages=[fuselage]
+        ),
+        op_point=op_point,
+        xyz_ref=mass_props_TOGW.xyz_cg,
+        working_directory=r"C:\Users\peter\Downloads\avl_debug"
+    ).run()
 
-def qp(*args):
-    p.qp(*[ss(x) for x in args], stacklevel=2)
+    import matplotlib.pyplot as plt
+    import aerosandbox.tools.pretty_plots as p
+    from aerosandbox.tools.string_formatting import eng_string
 
-# qp(root_chord, sink_rate)
+    ##### Section: Printout
+    print_title = lambda s: print(s.upper().join(["*" * 20] * 2))
 
+
+    def fmt(x):
+        return f"{s(x):.6g}"
+
+
+    print_title("Outputs")
+    for k, v in {
+        "mass_TOGW"             : f"{fmt(mass_props_TOGW.mass)} kg ({fmt(mass_props_TOGW.mass / u.lbm)} lbm)",
+        "L/D (actual)"          : fmt(LD_cruise),
+        "Cruise Airspeed"       : f"{fmt(op_point.velocity)} m/s",
+        "Cruise AoA"            : f"{fmt(op_point.alpha)} deg",
+        "Cruise CL"             : fmt(aero['CL']),
+        "Sink Rate"             : f"{fmt(sink_rate)} m/s",
+        "Cma"                   : fmt(aero['Cma']),
+        "Cnb"                   : fmt(aero['Cnb']),
+        "Cm"                    : fmt(aero['Cm']),
+        "Wing Reynolds Number"  : eng_string(op_point.reynolds(wing.mean_aerodynamic_chord())),
+        "AVL: Cma"              : avl_aero['Cma'],
+        "AVL: Cnb"              : avl_aero['Cnb'],
+        "AVL: Cm"               : avl_aero['Cm'],
+        "AVL: Clb Cnr / Clr Cnb": avl_aero['Clb Cnr / Clr Cnb'],
+        "CG location"           : "(" + ", ".join([fmt(xyz) for xyz in mass_props_TOGW.xyz_cg]) + ") m",
+        "Wing Span"             : f"{fmt(wing_span)} m ({fmt(wing_span / u.foot)} ft)",
+    }.items():
+        print(f"{k.rjust(25)} = {v}")
+
+    fmtpow = lambda x: fmt(x) + " W"
+
+    print_title("Mass props")
+    for k, v in mass_props.items():
+        print(f"{k.rjust(25)} = {v.mass * 1e3:.2f} g ({v.mass / u.oz:.2f} oz)")
+
+    if make_plots:
+        ##### Section: Geometry
+        airplane.draw_three_view(show=False)
+        p.show_plot(tight_layout=False, savefig="figures/three_view.png")
+
+        ##### Section: Mass Budget
+        fig, ax = plt.subplots(figsize=(12, 5), subplot_kw=dict(aspect="equal"), dpi=300)
+
+        name_remaps = {
+            **{
+                k: k.replace("_", " ").title()
+                for k in mass_props.keys()
+            },
+        }
+
+        mass_props_to_plot = copy.deepcopy(mass_props)
+        if mass_props_to_plot["ballast"].mass < 1e-6:
+            mass_props_to_plot.pop("ballast")
+        p.pie(
+            values=[
+                v.mass
+                for v in mass_props_to_plot.values()
+            ],
+            names=[
+                n if n not in name_remaps.keys() else name_remaps[n]
+                for n in mass_props_to_plot.keys()
+            ],
+            center_text=f"$\\bf{{Mass\\ Budget}}$\nTOGW: {s(mass_props_TOGW.mass * 1e3):.2f} g",
+            label_format=lambda name, value, percentage: f"{name}, {value * 1e3:.2f} g, {percentage:.1f}%",
+            startangle=110,
+            arm_length=30,
+            arm_radius=20,
+            y_max_labels=1.1
+        )
+        p.show_plot(savefig="figures/mass_budget.png")
